ac32826cdbfc062b4ca80da064a3d7e0fb327e2b
Chanuth10 <it20074418@my.sliit.lk>
2022-11-11 13:25:35 +0530

    Adding all files

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from './services/auth.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  loggedIn: boolean;
+  loginStatusSubscription: Subscription;
+  title = 'Frontend';
+
+  constructor(private router: Router, private authService: AuthService) {
+    this.authService.init();
+    this.loginStatusSubscription = this.authService.getLoginStatus()
+      .subscribe(loginStatus => {this.loggedIn = loginStatus})
+  }
+
+  ngOnInit() {
+    if (!this.authService.isLoggedIn()) {
+      this.router.navigateByUrl('login');
+    } else {
+      this.loggedIn = true;
+    }
+  }
+
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,83 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
+import { HttpService } from './http.service';
+import { Observable, Subject } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
+import jwtDecode from 'jwt-decode';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private loginUrl = 'api/auth/login';
+  private userUrl = 'api/employees';
+  private user;
+  private loginStatusSubject = new Subject<boolean>();
+  private userSubject = new Subject<any>();
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    }),
+  }
+
+  constructor(
+    private httpService:HttpService<any>,
+    private cookieService: CookieService,
+    private router: Router
+  ) { }
+
+  init(): void {
+    if (this.isLoggedIn()) {
+      this.loggedIn();
+    }
+  }
+
+  login(credentials: Object): Observable<any> {
+    return this.httpService.save(this.loginUrl, credentials);
+  }
+
+  loadUserInfo() {
+    const tokenData: any = jwtDecode(this.cookieService.get('token'));
+    if (tokenData) {
+      this.httpService.getById(this.userUrl, tokenData.id).subscribe(data => {
+        if (data._id) {
+          this.user = data;
+          this.userSubject.next(this.user);
+        }
+      });
+    }
+  }
+
+  loggedIn(): void {
+    this.loginStatusSubject.next(true);
+    this.loadUserInfo();
+  }
+
+  isLoggedIn(): boolean {
+    const token = this.cookieService.get('token');
+    const tokenData: any = token && jwtDecode(token);
+    return tokenData && tokenData.id !== null;
+  }
+
+  getLoginStatus(): Observable<boolean> {
+    return this.loginStatusSubject.asObservable();
+  }
+
+  getUserInfo(): Observable<any> {
+    return this.userSubject.asObservable();
+  }
+
+  getUserObject() {
+    return this.user;
+  }
+
+  logout(): void {
+    this.loginStatusSubject.next(false);
+    this.userSubject.next({});
+    this.cookieService.delete('token');
+    this.router.navigateByUrl('login');
+  }
+
+}
